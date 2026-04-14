b71f2c5b3450160e09dfba0648678357e406001b
Mahigautam27 <mahigautam0052@gmail.com>
2026-04-14 17:04:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
+  res.send("Updated Version Deployed Successfully!");
 });
 
 app.listen(5000, () => {
